61e2571ead364417e36a5c97c2b69dfb468e2583
javi <narutojavibanda@gmail.com>
2022-06-01 11:21:13 +0200

    Create repositories

---
+++ service-lessons/src/main/java/moa/store/lessons/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package moa.store.lessons.repository;
+
+import moa.store.lessons.model.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
---
+++ service-lessons/src/main/java/moa/store/lessons/repository/LessonRepository.java
@@ -0,0 +1,9 @@
+package moa.store.lessons.repository;
+
+import moa.store.lessons.model.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+}
