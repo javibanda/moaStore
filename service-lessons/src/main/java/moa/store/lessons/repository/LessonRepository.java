900989e47e9f505af77c575c67e5a788767352b0
javi <narutojavibanda@gmail.com>
2022-06-02 13:07:06 +0200

    Create query

--- service-lessons/src/main/java/moa/store/lessons/repository/LessonRepository.java
+++ service-lessons/src/main/java/moa/store/lessons/repository/LessonRepository.java
@@ -2,8 +2,14 @@ package moa.store.lessons.repository;
 
 import moa.store.lessons.model.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+    @Query("select lesson from Lesson lesson where month(lesson.date) =:month")
+    List<Lesson> getLessonByMonth(@Param("month") int month);
 }
